f4d34b7bc6a3d5c831a5801daf09cca4521c728f
Giovanni D'Italia <naturalbornpixel@users.noreply.github.com>
2020-07-06 13:39:16 +0200

    update

---
+++ Assets/Scripts/rotate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    float x, y;
+    float t = 0.0f;
+    float waitingTime = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = Random.Range(0.0f, 1.0f);
+        y = Random.Range(0.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(x, y, 0);
+
+
+        if (Time.time > t) {
+            x = Random.Range(0.0f, 1.0f);
+            y = Random.Range(0.0f, 1.0f);
+            t += waitingTime;
+        }
+
+
+    }
+}
